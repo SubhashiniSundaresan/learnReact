74691d34d4da096293c696c75bc4f73a3a7c07ea
Subhashini Sundaresan <subhashinisundaresan@Subhashinis-MBP.fios-router.home>
2021-05-04 12:53:03 -0400

    chapter 3 express

---
+++ back-end-template/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { routes } from "./routes";
+
+const app = express();
+app.use(express.json());
+routes.forEach((route) => {
+  app[route.method](route.path, route.handler);
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
+});
